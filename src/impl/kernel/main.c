8117ec50f622ec02b41f4953e5ddbf6d92ed6a12
Fish-game-dev <andrewkawalec164@gmail.com>
2022-02-13 23:18:00 -0500

    added calculator app

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,4 +1,5 @@
 #include "nugget.h"
+#include "apps.h"
 
 void show_date_time();
 
@@ -25,19 +26,37 @@ void kernel_main()
     print_s("Detected CPU: ");
     print_s(vendor_str);
 
-    print_s("\n>");
+    print_s("\ncomp>");
+
+    pprint_str(35, 24, "comp mode", PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK);
 
     int max_len = 50;
     char str[max_len];
 
     while (1)
     {
-        if (input_str(str, max_len))
+        if (input_str(str, max_len, true))
         {
             if (ic_strcmp(str, "clear") == 0)
             {
                 clr_scr();
-                print_s(">");
+                print_s("comp>");
+            }
+            else if (ic_strcmp(str, "calc") == 0)
+            {
+                clr_scr();
+                math();
+
+                clr_scr();
+                print_s("comp>");
+            }
+            else if (ic_strcmp(str, "help") == 0)
+            {
+                set_print_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
+                print_s("\nclear: clears screen");
+                print_s("\nclac: opens calculator mode");
+                set_print_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
+                print_s("\ncomp>");
             }
             else
             {
@@ -45,28 +64,11 @@ void kernel_main()
                 print_s("\nerror not valid command: ");
                 print_s(str);
                 set_print_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-                print_s("\n>");
+                print_s("\ncomp>");
             }
         }
 
         show_date_time();
         sleep(500000);
     }
-}
-
-void show_date_time()
-{
-    struct rtcdate r;
-    cmostime(&r);
-    pprint_int(0, 24, r.year, PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    pprint_char(4, 24, '-', PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    pprint_int_pad0(5, 24, r.month, 2, PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    pprint_char(7, 24, '-', PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    pprint_int_pad0(8, 24, r.day, 2, PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-
-    pprint_int_pad0(11, 24, r.hour, 2, PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    pprint_char(13, 24, ':', PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    pprint_int_pad0(14, 24, r.minute, 2, PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    pprint_char(16, 24, ':', PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    pprint_int_pad0(17, 24, r.second, 2, PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
 }
